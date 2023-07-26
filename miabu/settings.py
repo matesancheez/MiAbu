93a788a62e0122a4946a0c7a6bb06d6b5aa93de8
Mateo <sanchezmateo090@gmail.com>
2023-07-26 11:51:36 -0300

    arreglando errores csrf token 1

--- miabu/settings.py
+++ miabu/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-au_mi28v+gh&mk#fr2f%yq)rw#4cz*nbecj)vdyxjyw3ng9@hf"
+SECRET_KEY = "au_mi28v+gh&mk#fr2f%yq)rw#4cz*nbecj)vdyxjyw3ng9@hf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
