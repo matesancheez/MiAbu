25f5f8ba6ce5a798f8290fc2a50227a4471ca27d
Mateo <sanchezmateo090@gmail.com>
2023-07-26 11:36:58 -0300

    terminando de agregar archivos faltantes

--- usuario/forms.py
+++ usuario/forms.py
@@ -2,7 +2,8 @@ from usuario.models import Gasto
 from django import forms
 from django.forms import ModelForm
 
+
 class GastoForm(ModelForm):
     class Meta:
         model = Gasto
-        fields = '__all__'
+        fields = "__all__"
--- usuario/views.py
+++ usuario/views.py
@@ -15,96 +15,112 @@ class view_detalle(DetailView):
 @login_required
 def view_home(request):
     jubilacion = Jubilacion.objects.all()
-    jubilacion_int = Jubilacion.objects.aggregate(total=Sum('total'))['total']
+    jubilacion_int = Jubilacion.objects.aggregate(total=Sum("total"))["total"]
 
     gastos = Gasto.objects.all()
-    total_gastos = Gasto.objects.aggregate(total=Sum('monto'))['total']
+    total_gastos = Gasto.objects.aggregate(total=Sum("monto"))["total"]
 
-    if request.method == 'POST':
-        mes_seleccionado = request.POST.get('mes')
+    if request.method == "POST":
+        mes_seleccionado = request.POST.get("mes")
 
-        return redirect('/home/{}'.format(mes_seleccionado))
-    
+        return redirect("/home/{}".format(mes_seleccionado))
+
     if total_gastos == None:
-        total_gastos  = 0
-    
+        total_gastos = 0
+
     if jubilacion_int == None:
         jubilacion_int = 0
 
     jubilacion_restante = jubilacion_int - total_gastos
-    
-    return render(request, 'templates/pages/home.html', {'gastos_total': total_gastos,'gastos': gastos, 'jubilacion': jubilacion_int, 'restante': jubilacion_restante})
+
+    return render(
+        request,
+        "templates/pages/home.html",
+        {
+            "gastos_total": total_gastos,
+            "gastos": gastos,
+            "jubilacion": jubilacion_int,
+            "restante": jubilacion_restante,
+        },
+    )
 
 
 @login_required
 def view_home2(request, mes):
     gastos_mes = Gasto.objects.filter(mes=mes)
-    total_gastos_mes = gastos_mes.aggregate(total = Sum('monto'))['total']
-    
+    total_gastos_mes = gastos_mes.aggregate(total=Sum("monto"))["total"]
+
     jubilacion = Jubilacion.objects.all()
     jubilacion_mes = Jubilacion.objects.filter(mes=mes)
-    jubilacion_int = jubilacion_mes.aggregate(total=Sum('total'))['total']
+    jubilacion_int = jubilacion_mes.aggregate(total=Sum("total"))["total"]
 
+    if request.method == "POST":
+        mes_seleccionado = request.POST.get("mes")
 
-    if request.method == 'POST':
-        mes_seleccionado = request.POST.get('mes')
-
-        return redirect('/home/{}'.format(mes_seleccionado))
+        return redirect("/home/{}".format(mes_seleccionado))
 
     if total_gastos_mes == None:
-        total_gastos_mes  = 0
-    
+        total_gastos_mes = 0
+
     if jubilacion_int == None:
         jubilacion_int = 0
 
-    restante = jubilacion_int - total_gastos_mes 
+    restante = jubilacion_int - total_gastos_mes
 
-    return render(request, 'templates/pages/home2.html', {'gastos_total': total_gastos_mes ,'gastos': gastos_mes, 'jubilacion': jubilacion, 'restante': restante, 'jub_mes':jubilacion_int, 'gasto_mes':mes})
+    return render(
+        request,
+        "templates/pages/home2.html",
+        {
+            "gastos_total": total_gastos_mes,
+            "gastos": gastos_mes,
+            "jubilacion": jubilacion,
+            "restante": restante,
+            "jub_mes": jubilacion_int,
+            "gasto_mes": mes,
+        },
+    )
 
 
 def eliminar_gasto(request, pk):
     gasto = Gasto.objects.get(pk=pk)
     gasto.delete()
 
-    return redirect('home')
-    
+    return redirect("home")
+
 
 class editar_gasto(UpdateView):
-    template_name = 'editar_gasto.html'
+    template_name = "editar_gasto.html"
     queryset = Gasto.objects.all()
-    fields = ['nombre', 'monto', 'categoria', 'cuenta', 'mes', 'comprobante']
-    success_url = './'
+    fields = ["nombre", "monto", "categoria", "cuenta", "mes", "comprobante"]
+    success_url = "./"
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        messages.success(self.request, '¡Se ha editado correctamente!')
+        messages.success(self.request, "¡Se ha editado correctamente!")
         return response
-
 
     def form_invalid(self, form):
         response = super().form_invalid(form)
-        messages.error(self.request, '¡Ha ocurrido un error al editer el gasto!')
+        messages.error(self.request, "¡Ha ocurrido un error al editer el gasto!")
         return response
 
 
 class agregar_gasto(CreateView):
-    template_name = 'agregar_gasto.html'
+    template_name = "agregar_gasto.html"
     queryset = Gasto.objects.all()
-    fields = ['nombre', 'monto', 'categoria', 'cuenta', 'mes', 'comprobante']
-    success_url='./'
-
+    fields = ["nombre", "monto", "categoria", "cuenta", "mes", "comprobante"]
+    success_url = "./"
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        messages.success(self.request, '¡Se ha agregado correctamente!')
+        messages.success(self.request, "¡Se ha agregado correctamente!")
         return response
-
 
     def form_invalid(self, form):
         response = super().form_invalid(form)
-        messages.error(self.request, '¡Ha ocurrido un error al agregar el gasto!')
+        messages.error(self.request, "¡Ha ocurrido un error al agregar el gasto!")
         return response
-    
+
 
 class error_404(TemplateView):
-    template_name="errors/404.html"
+    template_name = "errors/404.html"
